58090b750f57083a39d1a49bb129e8e1cbd90bc6
DH <DH@macmac.skbroadband>
2024-05-03 09:06:35 +0900

    header

--- js/main.js
+++ js/main.js
@@ -586,4 +586,79 @@ window.addEventListener('DOMContentLoaded', function () {
       // markers: true,
     },
   });
+
+  const video10 = document.querySelector('#sc10_video');
+  const video11 = document.querySelector('#sc11_video');
+
+  function playVideoSec10(top, vidElmt) {
+    gsap.to('#section10', {
+      scrollTrigger: {
+        trigger: '#section10',
+        start: `${top} top`,
+        scrub: true,
+        // markers: true,
+        onEnter: () => {
+          vidElmt.play();
+        },
+
+        onLeaveBack: () => {
+          vidElmt.currentTime = 0; // 비디오 재생 시간을 0으로 되돌림
+          vidElmt.pause();
+        },
+      },
+    });
+  }
+
+  playVideoSec10('-500', video10);
+  playVideoSec10('1200', video11);
+
+  // javascript add or remove class :
+  // abc.classList.add('on) or abc.classList.remove('on')
+
+  // select element :
+  // document.querySelector('.abc');
+
+  // jQuery add or remove class:
+  // abc.addClass('on') or abc.removeClass('on')
+
+  // select element:
+  // $('.abc')
+
+  function animateTextSec10(top, elmt) {
+    gsap.to(elmt, {
+      scrollTrigger: {
+        trigger: '#section10',
+        start: `${top} top`,
+        scrub: true,
+        // markers: true,
+        onEnter: () => {
+          $(elmt).addClass('on');
+        },
+
+        onLeaveBack: () => {
+          $(elmt).removeClass('on');
+        },
+      },
+    });
+  }
+
+  animateTextSec10('400', '#section10 .page03');
+  animateTextSec10('2800', '#section10 .page05 .container01 .title');
+  animateTextSec10('3500', '#section10 .page05 .container02 .title');
+  animateTextSec10('4200', '#section10 .page05 .container03 .title');
+
+  function scrollMouse(evenOdd, height) {
+    gsap.to(`#section10 .page03 .mouse ${evenOdd}`, {
+      y: height * 2,
+      scrollTrigger: {
+        trigger: '#section10',
+        start: '200 top',
+        scrub: true,
+        // markers: true,
+      },
+    });
+  }
+
+  scrollMouse('.odd', -innerHeight);
+  scrollMouse('.even', innerHeight);
 });
--- js/setting.js
+++ js/setting.js
@@ -1,101 +1,82 @@
+// 변수 선언
 let container;
 let stage;
 
+// DOM이 완전히 로드된 후에 실행되는 함수
 window.addEventListener('DOMContentLoaded', function () {
+  // ScrollTrigger 플러그인을 gsap에 등록
   gsap.registerPlugin(ScrollTrigger);
 
+  // #container 요소를 stage 변수에 할당
   stage = document.querySelector('#container');
+  // Scrollbar를 초기화하고 container 변수에 할당
   container = Scrollbar.init(stage, {
-    damping: 0.1,
-    delegateTo: document,
-    continuousScrolling: true,
-    alwaysShowTracks: false,
+    damping: 0.1, // 스크롤 속도 감쇠 비율
+    delegateTo: document, // 스크롤 이벤트를 document에 위임
+    continuousScrolling: true, // 연속 스크롤링 활성화
+    alwaysShowTracks: false, // 스크롤 트랙을 항상 보이게 할지 여부
   });
 
+  // #container 요소에 대한 ScrollTrigger 프록시 설정
   ScrollTrigger.scrollerProxy('#container', {
     scrollTop(value) {
+      // value가 주어진 경우, container의 scrollTop을 value로 설정
       if (arguments.length) {
         container.scrollTop = value;
       }
 
+      // container의 scrollTop을 반환
       return container.scrollTop;
     },
   });
+
+  // ScrollTrigger 업데이트 이벤트 리스너를 container에 추가
   container.addListener(ScrollTrigger.update);
+  // ScrollTrigger의 기본 scroller를 stage로 설정
   ScrollTrigger.defaults({ scroller: stage });
 
-  container.setPosition(0, 0);
+  // container의 위치를 (0, 0)으로 설정
+  // container.setPosition(0, 0);
+  // container의 x축 트랙 요소를 제거
   container.track.xAxis.element.remove();
-  // Scrollbar.detachStyle();
 
+  // .startPoint 요소를 클릭하면 container를 (0, 0) 위치로 스크롤
   $('.startPoint').click(function () {
     container.scrollTo(0, 0, 600, {
-      callback: () => console.log('done!'),
-      easing: easing.easeInOutCirc,
+      callback: () => console.log('done!'), // 스크롤 완료 후 콜백 함수
+      easing: easing.easeInOutCirc, // 스크롤 이징 함수
     });
   });
 
+  // 즉시 실행 함수
   (function () {
     let v = 0;
 
+    // .scroll-content 요소에 대한 gsap 애니메이션 설정
     gsap.to('.scroll-content', {
       scrollTrigger: {
-        trigger: '.scroll-content',
-        start: 'top top',
-        end: 'bottom bottom',
-        scrub: true,
+        trigger: '.scroll-content', // 트리거 요소
+        start: 'top top', // 시작 위치
+        end: 'bottom bottom', // 종료 위치
+        scrub: true, // 스크럽 활성화
         onUpdate: (self) => {
-          // console.log('progress:', self.progress);
-          // v = Math.floor(self.progress * 5);
-          // console.log(v)
+          // 업데이트 이벤트 핸들러
+          // 현재는 주석 처리되어 있음
         },
       },
     });
-
-    /*
-
-
-        gsap.to('.fix_section', {
-            rotation: 360,
-            // x: -innerWidth*3,
-            scrollTrigger: {
-                trigger: "#section02",
-                start: "top top",
-                // end: () => "+=" + 300,
-                end: 'bottom bottom',
-                pin: true,
-                // pinSpace:false,
-                pinSpacing: false,
-                scrub: true,
-                markers: true
-            }
-        });
-
-
-        let sc02 = gsap.timeline();
-        ScrollTrigger.create({
-            animation: sc02,
-            trigger: "#section02",
-            start: "top top",
-            end: "bottom bottom",
-            pin:true,
-            pinSpacing: false,
-            scrub: 1,
-        });
-
-        sc02.to(".box",{x:100},0);
-        sc02.to(".box2",{y:100},1);
-
-
-*/
   })();
 
-  // Only necessary to correct marker position - not needed in production
+  // gsap-marker-scroller-start 클래스를 가진 요소가 있을 경우
   if (document.querySelector('.gsap-marker-scroller-start')) {
+    // gsap-marker 클래스를 포함하는 모든 요소를 배열로 변환
     const markers = gsap.utils.toArray('[class *= "gsap-marker"]');
 
+    // container에 offset 변경 이벤트 리스너를 추가
     container.addListener(({ offset }) => {
+      // 마커의 marginTop을 -offset.y로 설정
       gsap.set(markers, { marginTop: -offset.y });
+      // .posNum 요소의 내용을 offset.y로 설정
       $('.posNum').html(offset.y);
     });
   }
